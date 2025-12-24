b4cc03d5651375e75f1c55213f02b1f1faea9da0
DuyThong <duythongtrannguyen98@gmail.com>
2025-12-24 11:07:29 +0700

    user update for admin and user

--- Viren.Services/Dtos/Response/UserWithSubscriptionResponseDto.cs
+++ Viren.Services/Dtos/Response/UserWithSubscriptionResponseDto.cs
@@ -5,6 +5,9 @@ public class UserWithSubscriptionResponseDto
     public Guid Id { get; set; }
     public string Email { get; set; } = null!;
     public string Name { get; set; } = null!;
+
+    public string?  LastName { get; set; } 
+    public string? FirstName { get; set; } 
     public string? PhoneNumber { get; set; }
     public List<string> ? SubscriptionName { get; set; }
     
